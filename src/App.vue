a862b8774cf32f960e3910c683a497da3113811e
Raymond Nazaryan <rayn2427@gmail.com>
2018-04-25 21:16:39 -0700

    Added complex values to be passed to directives

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Custom Directives</h1>
                 <p v-highlight:background.delayed="'red'">Color this</p>
-                <p v-local-highlight:background.delayed.blink="'red'">Color this, too</p>
+                <p v-local-highlight:background.delayed.blink="{mainColor: 'red', secondColor: 'green', delay: 500}">Color this, too</p>
             </div>
         </div>
     </div>
@@ -25,11 +25,11 @@
                 bind(el, binding, vnode) {
                     var delay = 0;
                     if (binding.modifiers['delayed']) {
-                    delay = 3000;
+                        delay = 3000;
                     }
                     if (binding.modifiers['blink']) {
-                        let mainColor = binding.value;
-                        let secondColor = 'blue';
+                        let mainColor = binding.value.mainColor;
+                        let secondColor = binding.value.secondColor;
                         let currentColor = mainColor;
                         setTimeout(() => {
                             setInterval(() => {
@@ -39,15 +39,14 @@
                                 } else {
                                     el.style.color = currentColor;
                                 }
-                            }
-                            , 1000); 
+                            }, binding.value.delay); 
                         }, delay);
                     } else {
                         setTimeout(() => {
                             if (binding.arg == 'background') {
-                                el.style.backgroundColor = binding.value;
+                                el.style.backgroundColor = binding.value.mainColor;
                             } else {
-                                el.style.color = binding.value;
+                                el.style.color = binding.value.mainColor;
                             }
                         }, delay);
                     }
